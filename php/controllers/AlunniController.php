4ad90a97beebe550fab02a18806e329b02be598a
zoryon <gioelespata@gmail.com>
2025-03-11 17:20:50 +0100

    boh

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -26,10 +26,36 @@ class AlunniController
   public function create(Request $request, Response $response, $args) {
     $data = $request->getParsedBody();
 
+    $id = $data["id"];
+    $nome = $data["nome"];
+    $cognome = $data["cognome"];
+
     $mysqli_connection = new MySQLi("my_mariadb", "root", "ciccio", "scuola");
-    $mysqli_connection->query("INSERT INTO alunni(id, nome, cognome) VALUES(". $data["id"] . ", " . $data["nome"] . ", " . $_POST["cognome"] .")");
+    $mysqli_connection->query("INSERT INTO alunni(id, nome, cognome) VALUES(" . $id . ", '" .$nome . "', '" . $cognome . "')");
 
     $response->getBody()->write(json_encode("success"));
-    return $response->withHeader("Content-type", value: "application/json")->withStatus(200);
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function update(Request $request, Response $response, $args) {
+    $data = $request->getParsedBody();
+
+    $id = $data["id"];
+    $nome = $data["nome"];
+    $cognome = $data["cognome"];
+
+    $mysqli_connection = new MySQLi("my_mariadb", "root", "ciccio", "scuola");
+    $mysqli_connection->query("UPDATE alunni SET nome = '" . $nome . "', cognome = '" . $cognome . "' WHERE id = " . $id);
+
+    $response->getBody()->write(json_encode("success"));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function destroy(Request $request, Response $response, $args) {
+    $mysqli_connection = new MySQLi("my_mariadb", "root", "ciccio", "scuola");
+    $mysqli_connection->query("DELETE FROM alunni WHERE id = " . $args["id"]);
+
+    $response->getBody()->write(json_encode("success"));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
 }
--- php/index.php
+++ php/index.php
@@ -5,12 +5,13 @@ require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/controllers/AlunniController.php";
 
 $app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+$app->addBodyParsingMiddleware();
 
 $app->get("/alunni", "AlunniController:index");
 $app->get("/alunni/{id}", "AlunniController:view");
 $app->post("/alunni", "AlunniController:create");
 $app->put("/alunni/{id}", "AlunniController:update");
 $app->delete("/alunni/{id}", "AlunniController:destroy");
-
 
 $app->run();
